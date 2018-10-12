b99198e25bfdf20af6f87b39505808cc925dae66
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-10-13 01:26:22 +0200

    Script is now splitting logs
    Very basic low tech solution.

--- LogSep.py
+++ LogSep.py
@@ -1,11 +1,25 @@
 import re
 
 logFile = open("E:\\Blizzard\\World of Warcraft\\Logs\\WoWCombatLog.txt", "r", encoding='utf8')
+logsPath = "H:\\Github\\WarcraftLogs\\TestLogs\\"
 
-Deaths = 0
+encounterName = ""
+ID = 0
+combat = 0
+
 for line in logFile:
-    if "UNIT" in line:
-        if "Magequest" in line:
-            Deaths += 1
-
-print(Deaths)
+    if "ENCOUNTER_START" in line:
+        # Create var for file
+        encounterName = re.search('.*ENCOUNTER_START,.*"(.*)",.*', line).group(1)
+        combat = 1
+        ID += 1
+        encounterFile = open(logsPath+str(ID)+'_'+str(encounterName)+'.txt', 'w+')
+        print(encounterName)
+    if "ENCOUNTER_END" in line:
+        # Empty var for file
+        combat = 0
+        encounterFile.write(line)
+        encounterFile.close()
+    if combat == 1:
+        # Write to file
+        encounterFile.write(line)
