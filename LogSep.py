43f47e3c10de1ac8e0510bffe00a5177f9952398
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-10-14 20:22:22 +0200

    Working version
    Added functions.
    Split the files by date and hour/minutes.
    Cleaned up the file names.

--- LogSep.py
+++ LogSep.py
@@ -1,20 +1,28 @@
 import re
 
-logFile = open("E:\\Blizzard\\World of Warcraft\\Logs\\WoWCombatLog.txt", "r", encoding='utf8')
-logsPath = "H:\\Github\\WarcraftLogs\\TestLogs\\"
+logFile = open("D:\\Blizzard\\World of Warcraft\\Logs\\WoWCombatLog.txt", "r", encoding='utf8')
 
 encounterName = ""
 ID = 0
 combat = 0
 
+def getEncounterName():
+    return re.search('(.*:..):.*ENCOUNTER_START.*"(.*)",.*', line)
+
+def createEncounterFile():
+    logsPath = "H:\\Github\\WarcraftLogs\\TestLogs\\"
+    date = str(encounterName.group(1)).replace('/', '_').replace(' ', '_').replace(':', '')
+    name = str(encounterName.group(2)).replace(' ', '_')
+    return open(logsPath+date+'_'+name+'.txt', 'w+')
+
 for line in logFile:
     if "ENCOUNTER_START" in line:
         # Create var for file
-        encounterName = re.search('.*ENCOUNTER_START,.*"(.*)",.*', line).group(1)
+        encounterName = getEncounterName()
         combat = 1
         ID += 1
-        encounterFile = open(logsPath+str(ID)+'_'+str(encounterName)+'.txt', 'w+')
-        print(encounterName)
+        encounterFile = createEncounterFile()
+        print(encounterName.group(2))
     if "ENCOUNTER_END" in line:
         # Empty var for file
         combat = 0
